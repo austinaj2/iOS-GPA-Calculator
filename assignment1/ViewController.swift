8740ec5e20e69b26f822e5098b75eca2402ff93a
Andre Austin <austinaj2@vcu.edu>
2022-02-15 19:53:28 -0500

    Initial Commit

---
+++ assignment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment1
+//
+//  Created by Macky Cisse on 2/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
