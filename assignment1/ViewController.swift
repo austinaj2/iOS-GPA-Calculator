fae0791b4d9ca4012bc72c509620248610d6e1d3
Andre Austin <austinaj2@vcu.edu>
2022-02-17 19:19:09 -0500

    new

--- assignment1/ViewController.swift
+++ assignment1/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var courseTitle: UITextField!
+    @IBOutlet weak var assignPoints: UITextField!
+    @IBOutlet weak var midtermPoints: UITextField!
+    @IBOutlet weak var finalPoints: UITextField!
+    @IBOutlet weak var assignMax: UITextField!
+    @IBOutlet weak var midtermMax: UITextField!
+    @IBOutlet weak var finalMax: UITextField!
+    @IBOutlet weak var assignPctTotal: UITextField!
+    @IBOutlet weak var midtermPctTotal: UITextField!
+    @IBOutlet weak var finalPctTotal: UITextField!
+    @IBOutlet weak var creditsField: UITextField!
+    @IBOutlet weak var deleteCourseID: UITextField!
+    
+    @IBOutlet weak var addCourseBtn: UIButton!
+    @IBOutlet weak var deleteCourseBtn: UIButton!
+    
+    var mCourseText = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        courseTitle.delegate = self
+        assignPoints.delegate = self
+        midtermPoints.delegate = self
+        finalPoints.delegate = self
+        assignMax.delegate = self
+        midtermMax.delegate = self
+        finalMax.delegate = self
+        assignPctTotal.delegate = self
+        midtermPctTotal.delegate = self
+        finalPctTotal.delegate = self
+        creditsField.delegate = self
+        deleteCourseID.delegate = self
     }
 
+    @IBAction func addCourseClicked(_ sender: Any) {
+        if let name = courseTitle.text {
+            
+        }
+    }
+    
 
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        courseTitle.resignFirstResponder()
+        return true
+    }
+}
